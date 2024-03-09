691109ead71d73a0885b8c2042541d2d1baf8509
Kevin Escoto <96765154+silentznacht@users.noreply.github.com>
2024-03-09 15:08:43 -0500

    Swerve Implements =? Demo

--- Demo/src/main/java/frc/robot/Robot.java
+++ Demo/src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.XboxController;
@@ -31,59 +32,25 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+    // FRONT
+    private final CANSparkMax frontLeftDrive = new CANSparkMax(0, MotorType.kBrushless);
+    private final CANSparkMax frontRightDrive = new CANSparkMax(1, MotorType.kBrushless);
 
-  // RIGHT MOTORS
-    private final CANSparkMax rightMotor1 = new CANSparkMax(5, MotorType.kBrushed);
-    private final CANSparkMax rightMotor2 = new CANSparkMax(6, MotorType.kBrushed);
+   // BACK
+    private final CANSparkMax rearLeftDrive = new CANSparkMax(2, MotorType.kBrushless);
+    private final CANSparkMax rearRightDrive = new CANSparkMax(3, MotorType.kBrushless);
 
-  // LEFT MOTORS
-    private final CANSparkMax leftMotor1 = new CANSparkMax(7, MotorType.kBrushed);
-    private final CANSparkMax leftMotor2 = new CANSparkMax(8, MotorType.kBrushed);
-
+   // CONTROLLERS (Operator / Driver)
+    private final Joystick joystick = new Joystick(4);
+    private final XboxController userDriver = new XboxController(5);
   // UPPER-MECHANISM
-    private final CANSparkMax inOutSysMotor = new CANSparkMax(9, MotorType.kBrushed);
-    private final CANSparkMax hook = new CANSparkMax(10, MotorType.kBrushed);
+    private final CANSparkMax inOutSysMotor = new CANSparkMax(6, MotorType.kBrushless);
+    private final CANSparkMax hook = new CANSparkMax(7, MotorType.kBrushless);
 
   // Drive 
-  private DifferentialDrive m_Drive = new DifferentialDrive(leftMotor1, rightMotor1);
+    private MecanumDrive drive = new MecanumDrive(frontLeftDrive, rearLeftDrive, frontRightDrive, rearRightDrive);
 
-  /*
-   * Mechanism 1 (Upper): Sucks in & Out the ring
-   * Mechanism 2 (Upper):  Hoops up
-   */
-
-  // CONTROLLERS
-    private final XboxController userDriver = new XboxController(11);
-
-
-
-  // Constants used to substitute powers or speeds
-  static final double INTAKE_OUT_SPEED = 1.0; // percentage of feeder expelling note
-
-  static final double INTAKE_IN_SPEED = -.4; // percentage of speed feeder taking in note
-
-
-  static final int DRIVE_CURRENT_LIMIT_A = 60;  // drive train motor limit
-  static final int DRIVE_ROTATE_SPEED = .3; 
-  static final int DRIVE_POWER_SPEED = .60;
-  static final int DRIVE_TURN_SPEED = .3;
-
-
-  static final int FEEDER_CURRENT_LIMIT_A = 80; // feeder current limit
-
-  static final double FEEDER_AMP_SPEED = .4; // Percent output for amp or drop note, configure based on polycarb bend
-
-  static final int LAUNCHER_CURRENT_LIMIT_A = 80; //How many amps the launcher motor can use. lower to 60 if using neo
-
-  static final double LAUNCHER_SPEED = 1.0; // percentage output for when lauchers is intaking and expelling note
-
-  static final double LAUNCHER_AMP_SPEED = .17; // percentage output for scoring in amp or dropping note
-
-
-
-
-// below if robot does not work try changing the initallizing code 
-
+  
   @Override
   public void robotInit() {
 
@@ -93,58 +60,50 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {
-    // sets motors to rest
-    rightMotor1.set(0);
-    rightMotor2.set(0);
-    leftMotor1.set(0);
-    leftMotor2.set(0);
-    inOutSysMotor.set(0);
-    hook.set(0);
-  }
+  public void autonomousInit() {}
 
   @Override
   public void autonomousPeriodic() {}
 
   @Override
-  public void teleopInit() {
-    rightMotor1.set(0);
-    rightMotor2.set(0);
-    leftMotor1.set(0);
-    leftMotor2.set(0);
-    inOutSysMotor.set(0);
-    hook.set(0);
-  }
+  public void teleopInit() {}
 
   @Override
   public void teleopPeriodic() {
 
-      // Intake
-      if (userDriver.getRightBumperPressed()) { // powers intake for ring
-          inOutSysMotor.set(FEEDER_AMP_SPEED); // prone to change according to constants || For Reakab: Make sure to adjust this intake to an according speed, just like a motor
-      } else inOutSysMotor.set(0); // Sets intake to rest else
+      // INTAKE
+        if (userDriver.getRightBumperPressed()) { // powers intake for ring
+            inOutSysMotor.set(.6); // prone to change according to constants || For Reakab: Make sure to adjust this intake to an according speed, just like a motor
+        } else if (userDriver.getRightBumperReleased()) inOutSysMotor.set(0); // Sets intake to rest
 
-      // Hook
-      if (userDriver.getLeftBumperPressed()) {
-        hook.set(.3); // prone to change
-      } else hook.set(0);
+        // HOOK
+        if (userDriver.getLeftBumperPressed()) {
+          hook.set(.3); // prone to change
+        } else if (userDriver.getLeftBumperReleased()) inOutSysMotor.set(0);
 
+      // JOYSTICK INPUTS
+        double xSpeed = joystick.getRawAxis(1); // reads the input from the x-axis of the joystick | side-to-side motion or strafing
+        double ySpeed = joystick.getRawAxis(0); // reads the input from the y-axis of the joystick | forward-backward motion
+        double zRotation = joystick.getRawAxis(4); // reads the input from another axis of the joystick | rotation or turning
+        drive.driveCartesian(xSpeed, ySpeed, zRotation); // x - axis speed, y - axis speed, z-rotation speed
 
-      // Drive 
+      // Set steering angles for each wheqel module
+        setSteeringAngle(frontLeftDrive, joystick.getRawAxis(2));
+        setSteeringAngle(frontRightDrive, joystick.getRawAxis(3));
+        setSteeringAngle(rearLeftDrive, joystick.getRawAxis(2));
+        setSteeringAngle(rearRightDrive, joystick.getRawAxis(3));
+  }
+  
+  // method used to set steering angle for the swerve modules
+  private void setSteeringAngle(CANSparkMax rearLeftDrive2, double angleInput) {
+    // Convert joystick input to angle (assuming -1 to 1 input range)
+    double angle = angleInput * 90.0; // Adjust as needed based on joystick configuration
 
-      /*
-        Kevin E:
+    // Calculate speed control for the drive motor based on angle
+    double speed = Math.cos(Math.toRadians(angle));
 
-        Potential Issues & Solutions:
-          - If the movement of the robot is inversed or wonky, swap the x and y parameter (instead of .getY and then getX, swap it to .getX to .getY) 
-            Or it could be one of the -userDrives, since im not there to physically test it, you must find which motor ( left or right ) will start negative or positive
-            
-          - If the rotator button (B) does not work, try substituting it with another button availiable in the Xbox library configuration
-
-       */
-
-      // Allows robot to move and rotate  
-      m_Drive.arcadeDrive(userDriver.getY * DRIVE_POWER_SPEED, userDriver.getX * DRIVE_ROTATE_SPEED);
+    // Set the speed for the drive motor
+    rearLeftDrive2.set(speed);
   }
 
   @Override
